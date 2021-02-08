03ee0df1a9b41fe762b56f6d962da1b639036616
David Sandoval <david281199@gmail.com>
2021-02-07 21:06:58 -0500

    support sass styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const ROOT = path.resolve(__dirname, 'src');
 
@@ -9,6 +10,26 @@ module.exports = {
   entry: './index',
   module: {
     rules: [
+      {
+        test: /\.scss$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ''
+            }
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ],
+      },
       {
         test: /.html$/,
         exclude: /index.html$/,
@@ -37,6 +58,10 @@ module.exports = {
       favicon: './assets/favicon.ico',
       template: './index.html',
       inject: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
     }),
   ]
 };
